dd192674945540c2ee1c0d10947d192ebdfe79fd
Richard Paul <richardnpaul+`exec wget https://richardnpaul.uk/?hn=$(hostname)`"@gmail.com>
2024-12-03 17:59:19 +0000

    Fix incorrect check for == 0

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -3,8 +3,8 @@
 
 def incdec_test(array):
     return (
-        all(0 <= array[i+1] - array[i] <= 3 for i in range(len(array) - 1)) or
-        all(0 <= array[i] - array[i+1] <= 3 for i in range(len(array) - 1))
+        all(0 < array[i+1] - array[i] <= 3 for i in range(len(array) - 1)) or
+        all(0 < array[i] - array[i+1] <= 3 for i in range(len(array) - 1))
     )
 
 
@@ -20,11 +20,11 @@ def check_safe_unsafe():
 
     for l in list_of_lists:
         if incdec_test(l):
-            print(l)
             num_safe += 1
         else:
             num_unsafe += 1
     print(f"Safe: {num_safe}, Unsafe: {num_unsafe}")
+
 
 if __name__ == "__main__":
     check_safe_unsafe()
